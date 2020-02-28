9f420597861dbcdea2178c90fb271194d2104836
Yodhan1 <61077396+Yodhan1@users.noreply.github.com>
2020-02-28 19:46:07 +0700

    Update Soal2_enkripsi.sh

--- SOAL2/Soal2_enkripsi.sh
+++ SOAL2/Soal2_enkripsi.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#! /bin/bash
 Jam=`date '+%H'`
 String=${1%.*}
 Bawah="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
-atas=$Bawah | tr "a-z" "A-Z"
-#mv "$1" "$($String | tr "a-z" "${Bawah:$(Jam):26}" | tr "A-Z" "${Atas:$(Jam):26}").txt"
+Atas=`echo $Bawah | tr"a-z" "A-Z"`
+mv "$1" "$(echo $String | tr "a-z" "${Bawah:$Jam:26}" | tr "A-Z" "${Atas:$Jam:26}").txt"
+$Jam > $1
+
+
